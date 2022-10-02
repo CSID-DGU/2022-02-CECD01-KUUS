46a325553dc5a273bc9e6347329f6449bfb9b922
yujjineee <skekskekskek@gmail.com>
2022-10-02 18:27:35 +0900

    1002 Spring Local Server
    localhost:8080/hello.html
    로컬 작동 확인

---
+++ src/test/java/com/example/My/Trip/Flight/MyTripFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.My.Trip.Flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTripFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
